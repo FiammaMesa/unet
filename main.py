86291d77db03642529eb448064bd93d2ad566efa
FiammaMesa <fiamma1505@gmail.com>
2023-03-28 17:37:53 +0100

    Epochs and batch size setted.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+# %load_ext tensorboard
+import datetime
 from model import *
 from data import *
+# import torch
 from tensorflow.python.client import device_lib
+
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-
-# physical_devices = tf.config.list_physical_devices('GPU')
-# print("Num GPUs:", len(physical_devices))
-# print(device_lib.list_local_devices())
+# print ("---------- GPU torch --------------")
+# print (torch.cuda.is_available())
+# print (torch.cuda.device_count())
+# print (torch.cuda.current_device())
+print ("---------- GPU tensorflow --------------")
+physical_devices = tf.config.list_physical_devices('GPU')
+print("Num GPUs:", len(physical_devices))
+print(device_lib.list_local_devices())
 
 data_gen_args = dict(rotation_range=0.2,
                     width_shift_range=0.1,
@@ -18,14 +26,16 @@ data_gen_args = dict(rotation_range=0.2,
                     horizontal_flip=True,
                     vertical_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(16,r'data\train\image',r'data\train\label',data_gen_args,save_to_dir = 'results')
+myGene = trainGenerator(16,r'data\train',r'data\label',data_gen_args,save_to_dir = None)
 # myGene = trainGenerator(2,r'data\train\image',r'data\train\label',data_gen_args,save_to_dir = 'results')
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_plastic.hdf5', monitor='loss',verbose=1, save_best_only=True)
 # early_stopping = EarlyStopping(monitor="loss", min_delta=0.01, patience=2, verbose=1, mode="auto", baseline=None, restore_best_weights=True)
 # model.fit(myGene,epochs=4,callbacks=[model_checkpoint, early_stopping])
-model.fit(myGene,steps_per_epoch=1,epochs=4,callbacks=[model_checkpoint])
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+model.fit(myGene,steps_per_epoch=64,epochs=50,callbacks=[model_checkpoint, tensorboard_callback])
 
 testGene = testGenerator(r"data\test", num_image=50)
 results = model.predict(testGene,50,verbose=1)
